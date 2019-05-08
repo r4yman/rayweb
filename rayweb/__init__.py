ecdf3b52979756ecb725451cdeb7db98dd0f06b5
r4yman <wariusmalter@gmail.com>
2019-05-08 13:47:51 +0200

    fixed more errors

--- rayweb/__init__.py
+++ rayweb/__init__.py
@@ -29,8 +29,8 @@ def create_app(test_config=None):
 		return 'Hello, World!'
 
 	from . import db
-        db.init_db()
 	db.init_app(app)
+	db.init_db()
 
 	from . import auth
 	app.register_blueprint(auth.bp)
